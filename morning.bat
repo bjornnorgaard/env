0dde8a182cd13dab7a8f0e80de47921bbcc9c2d0
Bjørn Nørgaard <bno@it-minds.dk>
2019-04-02 16:02:50 +0200

    Updated morning bat file

--- morning.bat
+++ morning.bat
@@ -1,7 +1,9 @@
 @echo off
 
+start "" "C:\src\vito\Vito.Backend\Vito.sln"
 start "" "C:\Program Files\Double Commander\doublecmd.exe"
-
 start "" "C:\Program Files (x86)\KeePass Password Safe 2\KeePass.exe"
+start "" "https://bitbucket.org/itmindsdk/vito/commits/all"
+start "" "https://podio.com/home"
 
 exit
